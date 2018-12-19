4fe80b74a7e3a08dd917d9422ca44645f4cd7409
Lưu Thị Ngọc Lan <lanltn@nal.vn>
2018-12-19 17:26:42 +0700

    #create view

--- ImageCollapseView/app/src/main/java/com/lanltn/imagecollapseview/MainActivity.java
+++ ImageCollapseView/app/src/main/java/com/lanltn/imagecollapseview/MainActivity.java
@@ -3,11 +3,25 @@ package com.lanltn.imagecollapseview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageCollapsingView imageCollapsingView;
+    private int[] idImageList = {R.drawable.artist_1, R.drawable.artist_1, R.drawable.artist_1, R.drawable.artist_2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageCollapsingView = findViewById(R.id.imageCollapsingView);
+        List<Integer> idList = new ArrayList<>();
+        for (int i : idImageList) {
+            idList.add(i);
+        }
+        imageCollapsingView.setImageIdList(idList);
+
+
     }
 }
